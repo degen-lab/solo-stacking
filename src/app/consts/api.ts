19df776f0942c4db815ea0e106bfcc7359dd90a9
BowtiedDiego <bowtieddiego@gmail.com>
2025-01-28 14:04:18 +0200

    fix: update get-stacking-info calls to use dynamic contract based on network

--- src/app/consts/api.ts
+++ src/app/consts/api.ts
@@ -40,18 +40,21 @@ export const API_MEMPOOL_TRANSACTIONS_URL = (
   `https://api.${parseNetwork(
     network
   )}.hiro.so/extended/v1/address/${address}/mempool?unanchored=true`;
-export const API_STACKER_INFO_URL = (
-  network: "nakamoto-testnet" | "testnet" | "mainnet"
-) =>
-  `https://api.${parseNetwork(
-    network
-  )}.hiro.so/v2/contracts/call-read/ST000000000000000000002AMW42H/pox-4/get-stacker-info`;
 
 export const POX_4_CONTRACT_ADDRESS = (network: string) =>
   network === "mainnet"
     ? "SP000000000000000000002Q6VF78"
     : "ST000000000000000000002AMW42H";
 export const POX_4_CONTRACT_NAME = "pox-4";
+
+export const API_STACKER_INFO_URL = (
+  network: "nakamoto-testnet" | "testnet" | "mainnet"
+) =>
+  `https://api.${parseNetwork(
+    network
+  )}.hiro.so/v2/contracts/call-read/${POX_4_CONTRACT_ADDRESS(
+    network
+  )}/pox-4/get-stacker-info`;
 
 export const STACKS_NETWORK = (network: string) => {
   return network === "mainnet"
