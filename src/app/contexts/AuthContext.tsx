b04b1528b07dfb80e7f3a2a95bbb21531e5f9683
BowtiedDiego <bowtieddiego@gmail.com>
2025-09-11 17:52:49 +0300

    feat(auth-context): update to @stacks/connect v8

--- src/app/contexts/AuthContext.tsx
+++ src/app/contexts/AuthContext.tsx
@@ -1,39 +1,28 @@
 "use client";
-import { AppConfig, showConnect, UserSession } from "@stacks/connect";
+import {
+  connect,
+  disconnect,
+  getLocalStorage,
+  isConnected,
+} from "@stacks/connect";
 import { createContext, ReactNode } from "react";
 import { useNetwork } from "./NetworkContext";
+import { getUserAddress, getUserBtcAddress } from "../utils";
 
 export type Network = "mainnet" | "nakamoto-testnet" | "testnet";
 
 interface UserInterface {
-  stxAddress: {
-    mainnet: string;
-    testnet: string;
-  };
-  btcAddress: {
-    p2tr: {
-      mainnet: string;
-      testnet: string;
-      regtest: string;
-      simnet: string;
-    };
-    p2wpkh: {
-      mainnet: string;
-      testnet: string;
-      regtest: string;
-      simnet: string;
-    };
-  };
+  stxAddress: string | null;
+  btcAddress: string | null;
 }
 
 interface AuthContextInterface {
   user?: UserInterface | null;
   network: Network;
   btcNetwork: "mainnet" | "testnet";
-  userSession: UserSession;
   stxAddress: string | null;
   btcAddress: string | null;
-  walletProvider: ("leather" | "xverse") | undefined;
+  walletProvider: string | undefined;
   isAuthenticated: () => boolean;
   login: () => void;
   logout: () => void;
@@ -46,68 +35,50 @@ export const AuthContext = createContext<AuthContextInterface>(
 const AuthContextProvider: React.FC<{
   children: ReactNode;
 }> = ({ children }) => {
-  const appConfig = new AppConfig(["store_write", "publish_data"]);
   const { network } = useNetwork();
-  const userSession = new UserSession({ appConfig });
-
-  const isAuthenticated = () =>
-    userSession ? userSession.isUserSignedIn() : false;
-
-  const walletProvider: "leather" | "xverse" = isAuthenticated()
-    ? userSession.loadUserData().profile.walletProvider || "xverse"
+  const walletProvider = isConnected()
+    ? (getLocalStorage() as any)?.selectedProvider || "unknown"
     : undefined;
+
+  const isAuthenticated = () => {
+    return isConnected();
+  };
 
   const user = isAuthenticated()
     ? {
-        stxAddress: userSession.loadUserData().profile.stxAddress,
-        btcAddress: userSession.loadUserData().profile.btcAddress,
+        stxAddress: getUserAddress(),
+        btcAddress: getUserBtcAddress(),
       }
-    : null;
-
-  const stxAddress = user
-    ? user.stxAddress[network === "nakamoto-testnet" ? "testnet" : network]
-    : null;
-
-  // TODO: Asigna?
-  const btcAddress = user
-    ? walletProvider === "leather"
-      ? user.btcAddress.p2wpkh[
-          network === "nakamoto-testnet" ? "testnet" : network
-        ]
-      : user.btcAddress
     : null;
 
   // FIXME: Currently all Stacks networks work with the mainnet Bitcoin network.
   // However, Leather wallet won't let you pick the correct Bitcoin network.
   const btcNetwork = network === "mainnet" ? "mainnet" : "testnet";
 
-  const login = () => {
-    showConnect({
-      appDetails: {
-        name: "Degenlab Stacks Signer",
-        icon: window.location.origin + "/stacks-logo",
-      },
-      redirectTo: "/",
-      onFinish: () => {
-        window.location.reload();
-      },
-      userSession,
+  const login = async () => {
+    await connect({
+      walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID,
+      approvedProviderIds: [
+        "LeatherProvider",
+        "FordefiProvider.UtxoProvider",
+        "WalletConnectProvider",
+      ],
+      network,
     });
   };
 
-  const logout = () => {
-    userSession.signUserOut("/");
+  const logout = async () => {
+    await disconnect();
   };
 
   return (
     <AuthContext.Provider
       value={{
         user,
-        userSession,
+        stxAddress: user?.stxAddress || null,
+        btcAddress: user?.btcAddress || null,
         network,
         btcNetwork,
-        stxAddress,
-        btcAddress,
         walletProvider,
         isAuthenticated,
         login,
